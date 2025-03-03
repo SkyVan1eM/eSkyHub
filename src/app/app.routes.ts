b5dfff59a71d020e36b7c7bf0eaab7ecd23e95e9
SkyVan1eM <vadim.paduraru1@gmail.com>
2025-03-03 16:25:57 +0200

    add login/register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
